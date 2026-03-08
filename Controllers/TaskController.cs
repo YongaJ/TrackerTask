447b6646fa83a347dd74b2823e3209a928c2479a
Yonga Kamana <Yonga.Jacobs@eoh.com>
2026-03-08 20:45:42 +0200

    Adding Controller for actions: GetAll and GetById of tasks Added search optional parameters on getting of tasks

---
+++ Controllers/TaskController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using TaskTracker.Api.Data;
+using TrackerTask.Model;
+
+namespace TrackerTask.Controllers
+{
+    [ApiController]
+    [Route("api/tasks")]
+    public class TasksController : ControllerBase
+    {
+        private readonly DatabaseContext _db;
+
+        public TasksController(DatabaseContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllTasks(string? searchString)
+        {
+            try
+            {
+                var query = _db.Tasks.AsQueryable();
+
+                if (!string.IsNullOrWhiteSpace(searchString))
+                {
+                    query = query.Where(t =>
+                        t.Title.ToLower().Contains(searchString.ToLower()) ||
+                        (t.Description ?? "").ToLower().Contains(searchString.ToLower()));
+                }
+
+                return Ok(await query.ToListAsync());
+            }
+            catch (Exception ex)
+            {
+                return Problem(detail: ex.Message + ex?.InnerException);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetTaskById(int id)
+        {
+            var task = await _db.Tasks.FindAsync(id);
+
+            if (task == null)
+                return NotFound();
+
+            return Ok(task);
+        }
+
+    }
+}
