55a40f07ae3c46002b31895ebe7d6ecf3a4ad942
Yonga Kamana <Yonga.Jacobs@eoh.com>
2026-03-08 20:26:55 +0200

    Calling of seed data on startup Registering EF Core InMemory database Allowing only swagger calls

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+using System;
+using TaskTracker.Api.Data;
+using TaskTracker.Api.Seed;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,19 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DatabaseContext>(options =>
+    options.UseInMemoryDatabase("TaskDatabase"));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("https://localhost:7250/swagger",
+        policy =>
+        {
+            policy.AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
 
 var app = builder.Build();
 
@@ -21,5 +39,11 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<DatabaseContext>();
+    SeedingData.Seed(db);
+}
 
 app.Run();
