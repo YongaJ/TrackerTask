9267bc27639669900ba8cc8370b86ee7316fefc0
Yonga Kamana <Yonga.Jacobs@eoh.com>
2026-03-08 19:11:33 +0200

    Adding class items fro db linking

---
+++ Model/Item.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TrackerTask.Enums;
+
+namespace TrackerTask.Model
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; }
+
+        public TaskStatus Status { get; set; }
+
+        public Proirity Priority { get; set; }
+
+        public DateTime? DueDate { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
